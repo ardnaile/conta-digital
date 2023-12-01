b089c188c509e32f669910986dc520562395b048
Theo <69534996+TheoGomide@users.noreply.github.com>
2023-12-01 09:25:54 -0300

    FIX
    Correção nos valores atribuídos a FK num_conta

--- scripts-DML/transacao.sql
+++ scripts-DML/transacao.sql
@@ -1,17 +1,17 @@
 INSERT [Transacao] ([id_transacao], [valor_transacao], [dt_hr_transacao], [tp_transacao], [num_conta]) 
-VALUES (1, 1000.00, CAST(N'2023-01-05T14:30:00.000' AS DateTime), 1, 1);
+VALUES (1, 1000.00, CAST(N'2023-01-05T14:30:00.000' AS DateTime), 1, 123456);
 
 INSERT [Transacao] ([id_transacao], [valor_transacao], [dt_hr_transacao], [tp_transacao], [num_conta]) 
-VALUES (2, 500.50, CAST(N'2023-02-20T09:45:00.000' AS DateTime), 2, 2);
+VALUES (2, 500.50, CAST(N'2023-02-20T09:45:00.000' AS DateTime), 2, 987654);
 
 INSERT [Transacao] ([id_transacao], [valor_transacao], [dt_hr_transacao], [tp_transacao], [num_conta]) 
-VALUES (3, 1500.75, CAST(N'2023-03-15T16:20:00.000' AS DateTime), 3, 3);
+VALUES (3, 1500.75, CAST(N'2023-03-15T16:20:00.000' AS DateTime), 3, 567890);
 
 INSERT [Transacao] ([id_transacao], [valor_transacao], [dt_hr_transacao], [tp_transacao], [num_conta]) 
-VALUES (4, 200.20, CAST(N'2023-04-10T11:00:00.000' AS DateTime), 4, 4);
+VALUES (4, 200.20, CAST(N'2023-04-10T11:00:00.000' AS DateTime), 4, 109876);
 
 INSERT [Transacao] ([id_transacao], [valor_transacao], [dt_hr_transacao], [tp_transacao], [num_conta]) 
-VALUES (5, 750.90, CAST(N'2023-05-25T14:45:00.000' AS DateTime), 5, 5);
+VALUES (5, 750.90, CAST(N'2023-05-25T14:45:00.000' AS DateTime), 5, 234567);
 
 INSERT [Transacao] ([id_transacao], [valor_transacao], [dt_hr_transacao], [tp_transacao], [num_conta]) 
 VALUES (6, 300.30, CAST(N'2023-06-18T18:30:00.000' AS DateTime), 6, 6);
