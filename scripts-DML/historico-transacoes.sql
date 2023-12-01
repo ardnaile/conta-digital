81e834b2e22f55ec7c712096716d008014d4b09e
Theo <69534996+TheoGomide@users.noreply.github.com>
2023-12-01 09:31:28 -0300

    FIX
    Correção no valor atribuído a FK num_conta

--- scripts-DML/historico-transacoes.sql
+++ scripts-DML/historico-transacoes.sql
@@ -1,29 +1,29 @@
 INSERT [HistoricoTransacoes] ([id_historico], [num_conta], [id_transacao], [tp_transacao]) 
-VALUES (1, 1, 1, 1);
+VALUES (1, 123456, 1, 1);
 
 INSERT [HistoricoTransacoes] ([id_historico], [num_conta], [id_transacao], [tp_transacao]) 
-VALUES (2, 2, 2, 2);
+VALUES (2, 987654, 2, 2);
 
 INSERT [HistoricoTransacoes] ([id_historico], [num_conta], [id_transacao], [tp_transacao]) 
-VALUES (3, 3, 3, 3);
+VALUES (3, 567890, 3, 3);
 
 INSERT [HistoricoTransacoes] ([id_historico], [num_conta], [id_transacao], [tp_transacao]) 
-VALUES (4, 4, 4, 4);
+VALUES (4, 109876, 4, 4);
 
 INSERT [HistoricoTransacoes] ([id_historico], [num_conta], [id_transacao], [tp_transacao]) 
-VALUES (5, 5, 5, 5);
+VALUES (5, 234567, 5, 5);
 
 INSERT [HistoricoTransacoes] ([id_historico], [num_conta], [id_transacao], [tp_transacao]) 
-VALUES (6, 6, 6, 6);
+VALUES (6, 876543, 6, 6);
 
 INSERT [HistoricoTransacoes] ([id_historico], [num_conta], [id_transacao], [tp_transacao]) 
-VALUES (7, 7, 7, 7);
+VALUES (7, 543210, 7, 7);
 
 INSERT [HistoricoTransacoes] ([id_historico], [num_conta], [id_transacao], [tp_transacao]) 
-VALUES (8, 8, 8, 8);
+VALUES (8, 210987, 8, 8);
 
 INSERT [HistoricoTransacoes] ([id_historico], [num_conta], [id_transacao], [tp_transacao]) 
-VALUES (9, 9, 9, 9);
+VALUES (9, 987653, 9, 9);
 
 INSERT [HistoricoTransacoes] ([id_historico], [num_conta], [id_transacao], [tp_transacao]) 
-VALUES (10, 10, 10, 10);
+VALUES (10, 123459, 10, 10);
