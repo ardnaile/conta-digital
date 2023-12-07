cfee73d4dd65996bc79405dd2bdf52e77d33381d
Theo <69534996+TheoGomide@users.noreply.github.com>
2023-12-06 21:29:03 -0300

    FIX: Valores errados nas colunas num_cartao e num_conta

--- scripts-DML/cartao-debito.sql
+++ scripts-DML/cartao-debito.sql
@@ -1,29 +1,29 @@
 INSERT [CartaoDebito] ([num_cartao], [dt_validade], [cod_seguranca], [num_conta]) 
-VALUES (1111222233334444, '2024-05-01', 123, 1);
+VALUES ('1111222233334444', '2024-05-01', 123, 123456);
 
 INSERT [CartaoDebito] ([num_cartao], [dt_validade], [cod_seguranca], [num_conta]) 
-VALUES (2222333344445555, '2023-11-01', 456, 2);
+VALUES ('2222333344445555', '2023-11-01', 456, 987654);
 
 INSERT [CartaoDebito] ([num_cartao], [dt_validade], [cod_seguranca], [num_conta]) 
-VALUES (3333444455556666, '2025-02-01', 789, 3);
+VALUES ('3333444455556666', '2025-02-01', 789, 567890);
 
 INSERT [CartaoDebito] ([num_cartao], [dt_validade], [cod_seguranca], [num_conta]) 
-VALUES (4444555566667777, '2024-08-01', 234, 4);
+VALUES ('4444555566667777', '2024-08-01', 234, 109876);
 
 INSERT [CartaoDebito] ([num_cartao], [dt_validade], [cod_seguranca], [num_conta]) 
-VALUES (5555666677778888, '2023-01-01', 567, 5);
+VALUES ('5555666677778888', '2023-01-01', 567, 234567);
 
 INSERT [CartaoDebito] ([num_cartao], [dt_validade], [cod_seguranca], [num_conta]) 
-VALUES (6666777788889999, '2025-04-01', 890, 6);
+VALUES ('6666777788889999', '2025-04-01', 890, 876543);
 
 INSERT [CartaoDebito] ([num_cartao], [dt_validade], [cod_seguranca], [num_conta]) 
-VALUES (7777888899990000, '2024-10-01', 345, 7);
+VALUES ('7777888899990000', '2024-10-01', 345, 543210);
 
 INSERT [CartaoDebito] ([num_cartao], [dt_validade], [cod_seguranca], [num_conta]) 
-VALUES (8888999900001111, '2023-03-01', 678, 8);
+VALUES ('8888999900001111', '2023-03-01', 678, 210987);
 
 INSERT [CartaoDebito] ([num_cartao], [dt_validade], [cod_seguranca], [num_conta]) 
-VALUES (9999000011112222, '2025-06-01', 901, 9);
+VALUES ('9999000011112222', '2025-06-01', 901, 987657);
 
 INSERT [CartaoDebito] ([num_cartao], [dt_validade], [cod_seguranca], [num_conta]) 
-VALUES (1234123412345678, '2024-12-01', 234, 10);
+VALUES ('1234123412345678', '2024-12-01', 234, 123458);
