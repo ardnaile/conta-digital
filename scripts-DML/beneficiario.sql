db27b291e634fd901638432c8d8d7efc40a98ddf
Theo <69534996+TheoGomide@users.noreply.github.com>
2023-12-06 21:23:45 -0300

    REFACTOR: Reduzido o tamanho dos números da coluna num_conta_beneficiario

--- scripts-DML/beneficiario.sql
+++ scripts-DML/beneficiario.sql
@@ -1,29 +1,29 @@
 INSERT [Beneficiario] ([id_beneficiario], [nome_beneficiario], [num_conta_beneficiario], [banco], [id_usuario]) 
-VALUES (1, 'João Oliveira', 1234567890, 'Banco do Brasil', 1);
+VALUES (1, 'João Oliveira', 123456789, 'Banco do Brasil', 1);
 
 INSERT [Beneficiario] ([id_beneficiario], [nome_beneficiario], [num_conta_beneficiario], [banco], [id_usuario]) 
-VALUES (2, 'Camila Silva', 9876543210, 'Itau', 2);
+VALUES (2, 'Camila Silva', 987654321, 'Itau', 2);
 
 INSERT [Beneficiario] ([id_beneficiario], [nome_beneficiario], [num_conta_beneficiario], [banco], [id_usuario]) 
-VALUES (3, 'Rodrigo Santos', 5678901234, 'Santander', 3);
+VALUES (3, 'Rodrigo Santos', 567890123, 'Santander', 3);
 
 INSERT [Beneficiario] ([id_beneficiario], [nome_beneficiario], [num_conta_beneficiario], [banco], [id_usuario]) 
-VALUES (4, 'Amanda Costa', 1098765432, 'Bradesco', 4);
+VALUES (4, 'Amanda Costa', 109876543, 'Bradesco', 4);
 
 INSERT [Beneficiario] ([id_beneficiario], [nome_beneficiario], [num_conta_beneficiario], [banco], [id_usuario]) 
-VALUES (5, 'Bruno Lima', 2345678901, 'Banco do Brasil', 5);
+VALUES (5, 'Bruno Lima', 234567890, 'Banco do Brasil', 5);
 
 INSERT [Beneficiario] ([id_beneficiario], [nome_beneficiario], [num_conta_beneficiario], [banco], [id_usuario]) 
-VALUES (6, 'Gabriela Pereira', 8765432109, 'Itau', 6);
+VALUES (6, 'Gabriela Pereira', 876543210, 'Itau', 6);
 
 INSERT [Beneficiario] ([id_beneficiario], [nome_beneficiario], [num_conta_beneficiario], [banco], [id_usuario]) 
-VALUES (7, 'Lucas Oliveira', 5432109876, 'Santander', 7);
+VALUES (7, 'Lucas Oliveira', 543210987, 'Santander', 7);
 
 INSERT [Beneficiario] ([id_beneficiario], [nome_beneficiario], [num_conta_beneficiario], [banco], [id_usuario]) 
-VALUES (8, 'Mariana Costa', 2109876543, 'Bradesco', 8);
+VALUES (8, 'Mariana Costa', 210987654, 'Bradesco', 8);
 
 INSERT [Beneficiario] ([id_beneficiario], [nome_beneficiario], [num_conta_beneficiario], [banco], [id_usuario]) 
-VALUES (9, 'Rafael Pereira', 9876543210, 'Banco do Brasil', 9);
+VALUES (9, 'Rafael Pereira', 487654321, 'Banco do Brasil', 9);
 
 INSERT [Beneficiario] ([id_beneficiario], [nome_beneficiario], [num_conta_beneficiario], [banco], [id_usuario]) 
-VALUES (10, 'Thais Oliveira', 1234567890, 'Itau', 10);
+VALUES (10, 'Thais Oliveira', 823456789, 'Itau', 10);
