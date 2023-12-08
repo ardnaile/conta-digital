82cf176b4d87391b70409f4d2eb3f2b5bf87845d
Rafael Carvalho <122944870+Rafael171022@users.noreply.github.com>
2023-12-08 14:17:31 -0300

    Update PROC_NOVOUSU.sql
    Correção do PROC

--- procedures/PROC_NOVOUSU.sql
+++ procedures/PROC_NOVOUSU.sql
@@ -1,27 +1,18 @@
 CREATE PROCEDURE proc_novoUsu
-    @idUsuario INT,
-    @NomeCliente NVARCHAR(255),
-    @emailUsuario NVARCHAR(255),
-    @senhaUsuario NVARCHAR(255),
+    @NomeCliente VARCHAR(255),
+    @emailUsuario VARCHAR(255),
+    @senhaUsuario VARCHAR(255),
     @dt_cadastro DATETIME
 AS
 BEGIN
-    -- Verifica se o ID do usuário já existe
-    IF EXISTS (SELECT 1 FROM Usuario WHERE id_Usuario = @idUsuario)
-    BEGIN
-        -- Se o ID do usuário já existe, você pode lidar com isso da maneira desejada.
-        -- Pode lançar um erro, retornar uma mensagem, etc.
-        RAISERROR('ID do usuário já existe.', 16, 1);
-    END
-    ELSE
-    BEGIN
-        -- Insere o novo usuário
-        INSERT INTO Usuario (id_Usuario, nome_usuario, email_usuario, senha_usuario, data_registro)
-        VALUES (@idUsuario, @NomeCliente, @emailUsuario, @senhaUsuario, @dt_cadastro);
+     BEGIN
+        -- Insere o novo usuÃ¡rio
+        INSERT INTO Usuario ( nome_usuario, email_usuario, senha_usuario, data_registro)
+        VALUES ( @NomeCliente, @emailUsuario, @senhaUsuario, @dt_cadastro);
         
-        -- Se necessário, você pode fazer mais coisas aqui, como enviar notificações, etc.
+        -- Se necessÃ¡rio, vocÃª pode fazer mais coisas aqui, como enviar notificaÃ§Ãµes, etc.
         
-        -- Retorna uma mensagem indicando que o usuário foi criado com sucesso
-        SELECT 'Usuário criado com sucesso.' AS Mensagem;
+        -- Retorna uma mensagem indicando que o usuÃ¡rio foi criado com sucesso
+        SELECT 'UsuÃ¡rio criado com sucesso.' AS Mensagem;
     END
 END;
