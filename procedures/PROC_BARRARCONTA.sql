03ab6f84851b5f7a93dfd48de7c33175f7d0f52f
Rafael Carvalho <122944870+Rafael171022@users.noreply.github.com>
2023-12-08 14:49:28 -0300

    Update PROC_BARRARCONTA.sql

--- procedures/PROC_BARRARCONTA.sql
+++ procedures/PROC_BARRARCONTA.sql
@@ -2,10 +2,15 @@ CREATE PROCEDURE proc_barrarConta
     @NumConta INT
 AS
 BEGIN
-	IF EXISTS (SELECT 1 FROM Conta WHERE num_conta in (1,4,6,7))
-    BEGIN
-        --Caso a conta não exista sai da proc
-        RAISERROR('Esta conta esta bloqueada.', 16, 1);
-		RETURN; 
-    END
+	IF EXISTS (SELECT 1 FROM Conta WHERE num_conta = @NumConta and bloqueio <> 0)
+    	BEGIN
+		RAISERROR('Esta conta ja esta bloqueada.', 16, 1);
+		RETURN; 	
+	END
+	ELSE
+	BEGIN
+		UPDATE Conta SET conta_bloqueada = 1 WHERE num_conta = @NumConta;	
+	END
+	
 END
+
