ff459faaed5ff6f14699c1e6c647497f455900d5
Theo <69534996+TheoGomide@users.noreply.github.com>
2023-12-08 11:10:04 -0300

    REFACTOR: Data Gerada Automaticamente

--- scripts-DDl/historico-login.sql
+++ scripts-DDl/historico-login.sql
@@ -2,7 +2,7 @@
 CREATE TABLE [HistoricoLogin] (
   [id_login] int IDENTITY(1,1) PRIMARY KEY,
   [id_usuario] int,
-  [dt_hr_login] datetime,
+  [dt_hr_login] datetime DEFAULT(GETDATE()),
   [endereco_ip] varchar(255)
 )
 GO
