b024140fe79cfe26491673013444e41fd03c69f0
Rafael Carvalho <122944870+Rafael171022@users.noreply.github.com>
2023-12-08 10:49:23 -0300

    Update PROC_GERACARTAO.sql

--- PROC_GERACARTAO.sql
+++ PROC_GERACARTAO.sql
@@ -2,26 +2,29 @@ CREATE PROCEDURE proc_GerarCartaoDebito
     @NumConta INT
 AS
 BEGIN
+	--Verifica se a conta n√£o esta bloqueada
+	EXECUTE proc_barrarConta @NumConta;
+
 	IF NOT EXISTS (SELECT 1 FROM Conta WHERE num_conta = @NumConta)
     BEGIN
-        --Caso a conta n„o exista sai da proc
-        RAISERROR('Conta n„o encontrada.', 16, 1);
+        --Caso a conta n√£o exista sai da proc
+        RAISERROR('Conta n√£o encontrada.', 16, 1);
 		RETURN; 
     END
 
-    -- Gera n˙mero de cart„o aleatÛrio (16 dÌgitos)
+    -- Gera n√∫mero de cart√£o aleat√≥rio (16 d√≠gitos)
     DECLARE @NumCartao VARCHAR(16)
     SET @NumCartao = CAST(ABS(CHECKSUM(NEWID())) % 10000000000000000 AS VARCHAR(16))
 
-    -- Gera data de validade aleatÛria (para os prÛximos 5 anos a partir da data atual)
+    -- Gera data de validade aleat√≥ria (para os pr√≥ximos 5 anos a partir da data atual)
     DECLARE @DtValidade DATE
     SET @DtValidade = DATEADD(YEAR, ABS(CHECKSUM(NEWID())) % 5, GETDATE())
 
-    -- Gera cÛdigo de seguranÁa aleatÛrio (3 dÌgitos)
+    -- Gera c√≥digo de seguran√ßa aleat√≥rio (3 d√≠gitos)
     DECLARE @CodSeg INT
     SET @CodSeg = ABS(CHECKSUM(NEWID())) % 1000
 
-    -- Insere os dados na tabela de cartıes de dÈbito (substitua 'SuaTabela' pelo nome real da tabela)
+    -- Insere os dados na tabela de cart√µes de d√©bito (substitua 'SuaTabela' pelo nome real da tabela)
     INSERT INTO CartaoDebito(num_cartao, dt_validade, cod_seguranca, num_conta)
     VALUES (@NumCartao, @DtValidade, @CodSeg, @NumConta)
 END
