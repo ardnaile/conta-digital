5d4a2e587eb6ef2f4f6445aa0fc92b47fe0f96b4
Theo <69534996+TheoGomide@users.noreply.github.com>
2023-12-08 14:19:13 -0300

    FIX: token repetido

--- scripts-DDl/token-autenticacao.sql
+++ scripts-DDl/token-autenticacao.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [TokenAutenticacao] (
   [id_token] int IDENTITY(1,1) PRIMARY KEY,
   [id_usuario] int,
-  [token] [token] varchar(36) DEFAULT NEWID(),
+  [token] varchar(36) DEFAULT NEWID(),
   [dt_criacao] datetime DEFAULT(GETDATE()),
   [dt_expiracao] datetime DEFAULT(DATEADD(YEAR, 5, GETDATE()))
 )
